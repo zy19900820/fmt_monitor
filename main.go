4b98ba5862924306eebd1b1545e2e3ed0fe094c8
xie <809979396@qq.com>
2022-05-18 16:52:07 +0800

    fix final

--- main.go
+++ main.go
@@ -6,10 +6,61 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"golang.org/x/xerrors"
 )
+
+const DING_ERROR_URL = "https://oapi.dingtalk.com/robot/send?access_token=d5e507b4ef9122b60af42e7418050a67eb43d797315a2504469de01570684ba9"
+const ERROR_WORD = "提醒 "
+
+func dingInfo1(msg string, bAll bool) error {
+	client := &http.Client{}
+	type sendDing struct {
+		Msgtype string `json:"msgtype"`
+		Text    struct {
+			Content string `json:"content"`
+		} `json:"text"`
+		At struct {
+			IsAtAll bool `json:"isAtAll"`
+		} `json:"at"`
+	}
+	var cmd sendDing
+	cmd.Msgtype = "text"
+	cmd.Text.Content = ERROR_WORD + msg
+	cmd.At.IsAtAll = bAll
+	a, _ := json.Marshal(cmd)
+	req, err := http.NewRequest("POST", DING_ERROR_URL,
+		strings.NewReader(string(a)))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	res := struct {
+		Errcode int    `json:"errcode"`
+		Errmsg  string `json:"errmsg"`
+	}{}
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		return err
+	}
+	if res.Errmsg == "ok" {
+		return nil
+	} else {
+		return xerrors.New(res.Errmsg)
+	}
+}
 
 type PRICE_INFO struct {
 	Price string `json:"price"`
@@ -115,11 +166,15 @@ func main() {
 			continue
 		}
 
-		var lowPrice float64
-		var highPrice float64
+		lowPrice := price1
+		highPrice := price1
 		if price2 < price1 {
 			lowPrice = price2
 			highPrice = price1
+		}
+		if price3 < price2 {
+			lowPrice = price3
+			highPrice = price2
 		}
 		if price3 < price1 {
 			lowPrice = price3
@@ -127,5 +182,8 @@ func main() {
 		}
 		log.Println(lowPrice)
 		log.Println(highPrice)
+		if highPrice > lowPrice*1.05 {
+			dingInfo1("交易所ftm存在溢价", true)
+		}
 	}
 }
