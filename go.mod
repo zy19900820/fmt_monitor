fd436bc30d99230d79fa14282ff3d57f0801f13d
xie <809979396@qq.com>
2022-05-18 16:13:15 +0800

    get price

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ftm_monitor
+
+go 1.16
+
+require golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+
+	"golang.org/x/xerrors"
+)
+
+type PRICE_INFO struct {
+	Price string `json:"price"`
+}
+
+const SPOT_URL = "https://api1.binance.com/api/v3/ticker/price?symbol=FTMUSDT"
+const UST_FUTURES_URL = "https://fapi.binance.com/fapi/v1/ticker/price?symbol=FTMUSDT"
+
+//const COIN_FUTURES_URL = "https://dapi.binance.com/dapi/v1/ticker/price?symbol=FTMUSD_PERP" json结构不一样 不解析了
+
+func getFMTBinancePrice(url string) (float64, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	var priceInfo PRICE_INFO
+	err = json.Unmarshal(body, &priceInfo)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	price, err := strconv.ParseFloat(priceInfo.Price, 64)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	return price, nil
+}
+
+type OK_PRICE_INFO_DATA struct {
+	Last string `json:"last"`
+}
+
+type OK_PRICE_INFO struct {
+	Code string               `json:"code"`
+	Msg  string               `json:"msg"`
+	Data []OK_PRICE_INFO_DATA `json:"data"`
+}
+
+func getFMTOkPrice() (float64, error) {
+	resp, err := http.Get("https://www.okex.com/api/v5/market/ticker?instId=FTM-USDT-SWAP")
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	log.Print("Response: ", string(body))
+	var priceInfo OK_PRICE_INFO
+	err = json.Unmarshal(body, &priceInfo)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
+
+	if priceInfo.Code == "0" {
+		data := priceInfo.Data[0]
+		price, err := strconv.ParseFloat(data.Last, 64)
+		if err != nil {
+			log.Println(err)
+			return 0, err
+		}
+		return price, nil
+	} else {
+		return 0, xerrors.New(priceInfo.Msg)
+	}
+}
+
+func main() {
+	price, err := getFMTOkPrice()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(price)
+}
+
